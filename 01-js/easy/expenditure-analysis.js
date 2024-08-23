34ce9d6d23b9dd35879c60b09c2d46fc05f2c029
Pratik Yesane <pratiky2000@gmail.com>
2024-08-23 16:42:23 +0530

    Done

--- 01-js/easy/expenditure-analysis.js
+++ 01-js/easy/expenditure-analysis.js
@@ -7,9 +7,49 @@
   Once you've implemented the logic, test your code by running
   - `npm run test-expenditure-analysis`
 */
+const items = [
+  {
+    itemName: "Mobile Phone",
+    category: "Electronics",
+    price: 100,
+    timestamp: Date.now(),
+  },
+  {
+    itemName: "Shirt1",
+    category: "Cloths",
+    price: 500,
+    timestamp: Date.now(),
+  },
+  {
+    itemName: "Shirt2",
+    category: "Cloths",
+    price: 1100,
+    timestamp: Date.now(),
+  },
+  {
+    itemName: "Bed",
+    category: "Furniture",
+    price: 200,
+    timestamp: Date.now(),
+  },
+];
 
 function calculateTotalSpentByCategory(transactions) {
-  return [];
+  const cateoryTotalAmount = {};
+
+  transactions.forEach((transaction) => {
+    const { category, price } = transaction;
+    if (category in cateoryTotalAmount) {
+      cateoryTotalAmount[category] += price;
+    } else {
+      cateoryTotalAmount[category] = price;
+    }
+  });
+
+  return Object.keys(cateoryTotalAmount).map((category) => ({
+    [category]: cateoryTotalAmount[category],
+  }));
 }
 
+console.log(calculateTotalSpentByCategory(items));
 module.exports = calculateTotalSpentByCategory;
